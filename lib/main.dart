63be7a19d2bcb725ecb936b5888dc50b5ed1700f
Surbhi <surbhikumaridav@gmail.com>
2023-02-13 23:14:48 +0530

    structuring the app

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:great_places_app/providers/great_places.dart';
+import 'package:great_places_app/screens/places_list_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo)
+                .copyWith(secondary: Colors.amber)),
+        home: const PlacesListScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Places'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Center(child: CircularProgressIndicator(),),
+    );
+  }
+}
